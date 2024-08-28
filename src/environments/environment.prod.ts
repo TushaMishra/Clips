48a41446ae21a151fbaf5e00e3f16d3525ebcee6
Tusha Mishra <tushamishra123@gmail.com>
2024-08-28 17:10:26 +0530

    Integrated firebase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyBumSgT4hPEHpi_TjPFkOVP-e8NT0k-LHE",
+        authDomain: "clips-d89ad.firebaseapp.com",
+        projectId: "clips-d89ad",
+        storageBucket: "clips-d89ad.appspot.com",
+        appId: "1:698722736229:web:22c6393a51a171505a0e8c"
+      }
+};
