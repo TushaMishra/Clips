4bb22f8942fefc66aee5c94c61ad2cc95b2a64a7
Tusha Mishra <tushamishra123@gmail.com>
2024-08-29 07:47:55 +0530

    Set navbar on authentication

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { map, Observable } from 'rxjs';
 import IUser from 'src/app/models/user.model';
 
 @Injectable({
@@ -8,12 +9,17 @@ import IUser from 'src/app/models/user.model';
 })
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>
+  public isAuthenticated$: Observable<boolean>
 
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore
   ) { 
     this.usersCollection = db.collection('users')
+    auth.user.subscribe(console.log)
+    this.isAuthenticated$ = auth.user.pipe(
+      map(user => !!user)
+    )
   }
 
   public async createUser(userData: IUser) {
