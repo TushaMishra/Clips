993b626bccec34b66ff22200b9250cda8bf0642b
Tusha Mishra <tushamishra123@gmail.com>
2024-08-29 14:13:10 +0530

    Created custom error through factory function

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import IUser from 'src/app/models/user.model';
+import { RegisterValidators } from '../validators/register-validators';
 
 @Component({
   selector: 'app-register',
@@ -50,7 +51,7 @@ export class RegisterComponent {
     password: this.password,
     confirm_password: this.confirm_password,
     phoneNumber: this.phoneNumber
-  })
+  }, [RegisterValidators.match("password", "confirm_password")])
   async register() {
     this.showAlert = true
     this.alertMsg = 'Please wait! Your account is being created.'
