761dd3d3b6a4ada7faed8b52e775ccd706743710
Tusha Mishra <tushamishra123@gmail.com>
2024-09-06 14:43:14 +0530

    Make Manage page working, dynamic video player and copy link

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -5,13 +5,15 @@ import {
 } from '@angular/fire/compat/firestore';
 import IClip from '../models/clip.model';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { BehaviorSubject, combineLatest, map, of, switchMap } from 'rxjs';
+import { BehaviorSubject, combineLatest, map, Observable, of, switchMap } from 'rxjs';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
+import { snapshot } from 'node:test';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ClipService {
+export class ClipService implements Resolve<IClip | null> {
   pageClips: IClip[] = []
   pendingReq = false
 
@@ -19,7 +21,8 @@ export class ClipService {
   constructor(
     private db: AngularFirestore,
     private auth: AngularFireAuth,
-    private storage: AngularFireStorage
+    private storage: AngularFireStorage,
+    private router: Router
   ) { 
     this.clipsCollection = db.collection('clips')
   }
@@ -103,5 +106,25 @@ console.log(this.pageClips)
     })
 
     this.pendingReq = false
+  }
+
+  resolve(
+    route: ActivatedRouteSnapshot, 
+    state: RouterStateSnapshot
+  ) : IClip | Observable<IClip | null> | Promise<IClip | null> | null {
+      return this.clipsCollection.doc(route.params.id)
+        .get()
+        .pipe(
+          map(snapshot => {
+            const data = snapshot.data()
+            if(!data) {
+              this.router.navigate(['/'])
+              return null
+            }
+
+            return data
+          })
+
+        )
   }
 }
